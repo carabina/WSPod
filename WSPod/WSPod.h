fa99b757690e8cd2bec1e1606f4f4d741cdfe4a0
cyW0923 <835151791@qq.com>
2017-06-08 14:45:44 +0800

    init

---
+++ WSPod/WSPod.h
@@ -0,0 +1,13 @@
+//
+//  WSPod.h
+//  Demo
+//
+//  Created by wsong on 2017/6/8.
+//  Copyright © 2017年 wsong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WSPod : NSObject
+
+@end
---
+++ WSPod/WSPod.m
@@ -0,0 +1,13 @@
+//
+//  WSPod.m
+//  Demo
+//
+//  Created by wsong on 2017/6/8.
+//  Copyright © 2017年 wsong. All rights reserved.
+//
+
+#import "WSPod.h"
+
+@implementation WSPod
+
+@end
